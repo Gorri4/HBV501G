8cd0b956d13c43b5cc0a34eed27f0c6576afbfe3
Arnór Kristmundsson <arnorkr@gmail.com>
2017-11-08 17:53:08 +0000

    Forsíðu lokið í bili

--- webpage/modal.js
+++ webpage/modal.js
@@ -1,25 +1,69 @@
-// Get the openModal button
-var openModalBtn = document.getElementById('openModalLoginForm');
+// Get the openLogin button
+var openLoginBtn = document.getElementById('openLoginForm');
 
-//Get the closeModal button
-var closeModalBtn = document.getElementById('closeModalLoginForm');
+//Get the closeLogin button
+var closeLoginBtn = document.getElementById('closeLoginForm');
 
-// Get the modal
-var modal = document.getElementById('modalLoginForm');
+//Get the cancelLogin button
+var cancelLoginBtn = document.getElementById('cancelLogin');
 
-// When the user clicks on the openModal button, open the modal
-openModalBtn.onclick = function openModal() {
-    modal.style.display = "block";
+// Get the modalLoginForm
+var loginForm = document.getElementById('loginForm');
+
+// Get the openSignup button
+var openSignupBtn = document.getElementById('openSignupForm');
+
+//Get the closeSignup button
+var closeSignupBtn = document.getElementById('closeSignupForm');
+
+//Get the cancelSignup button
+var cancelSignupBtn = document.getElementById('cancelSignup');
+
+// Get the modalSignupForm
+var signupForm = document.getElementById('signupForm');
+
+openForm = function(form){
+	form.style.display = "block";
 }
 
-//When the user clicks on the closeModal button, close the modal
-closeModalBtn.onclick = function closeModal() {
-    modal.style.display = "none";
+closeForm = function(form){
+	form.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function closeModal(event) {
-    if (event.target == modal) {
-    	modal.style.display = "none";
-    }
+// When the user clicks on the openLogin button, open the modalLoginForm
+openLoginBtn.onclick = function() {
+	openForm(loginForm);
+}
+
+//When the user clicks on the closeLogin button, close the modalLoginForm
+closeLoginBtn.onclick = function() {
+	closeForm(loginForm);
+}
+
+//When the user clicks on the cancelLogin button, close the modalLoginForm
+cancelLoginBtn.onclick = function() {
+	closeForm(loginForm);
+}
+
+// When the user clicks on the openSignup button, open the modalSignupForm
+openSignupBtn.onclick = function() {
+	openForm(signupForm);
+}
+
+//When the user clicks on the closeSignup button, close the modalSignupForm
+closeSignupBtn.onclick = function() {
+	closeForm(signupForm);
+}
+
+//When the user clicks on the cancelSignup button, close the modalSignupForm
+cancelSignupBtn.onclick = function() {
+	closeForm(signupForm);
+}
+
+// When the user clicks anywhere outside of the form, close it
+window.onclick = function(event) {
+	if (event.target == loginForm)
+		closeForm(loginForm);
+	if (event.target == signupForm)
+		closeForm(signupForm);
 }
